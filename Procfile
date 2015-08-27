dbbdd4d1c1d2bf360ce84fd535d13b67a24898c5
mugur.rus <mugur.rus@sourcefabric.org>
2015-08-27 14:30:18 +0300

    lots of fixes for paths and repository split

--- Procfile
+++
@@ -1,3 +0,0 @@
-pubapi: gunicorn -c gunicorn_config.py wsgi
-work: celery -A worker worker
-beat: celery -A worker beat --pid=
---
+++ content_api/assets/util.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8; -*-
+#
+# This file is part of Superdesk.
+#
+# Copyright 2013, 2015 Sourcefabric z.u. and contributors.
+#
+# For the full copyright and license information, please see the
+# AUTHORS and LICENSE files distributed with this source code, or
+# at https://www.sourcefabric.org/superdesk/license
+
+from flask import url_for, current_app as app
+
+
+def url_for_media(media_id):
+    try:
+        url = app.media.url_for_media(media_id)
+        if url is None:
+            return url_for('assets.get_media_streamed', media_id=media_id, _external=True)
+    except AttributeError:
+        return url_for('assets.get_media_streamed', media_id=media_id, _external=True)

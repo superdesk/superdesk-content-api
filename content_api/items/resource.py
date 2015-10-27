60b8c9f34c7795a4409aff6e198f4f4d36cf4821
Petr Jasek <jasekpetr@gmail.com>
2015-10-27 14:23:37 +0100

    feat(items): add slugline to item metadata

--- content_api/items/resource.py
+++ content_api/items/resource.py
@@ -29,6 +29,7 @@ schema = {
     'profile': {'type': 'string'},
     'pubstatus': {'type': 'string'},
     'renditions': {'type': 'dict'},
+    'slugline': {'type': 'string'},
     'subject': {'type': 'list'},
     'type': {'type': 'string'},
     'urgency': {'type': 'integer'},
